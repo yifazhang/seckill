e03e597de8413c86f799e5865d6cc8cc8e22e269
yifazhang <grant2015@126.com>
2017-06-04 19:55:38 +0800

    Dao层测试完成

---
+++ src/test/java/org/seckill/dao/SuccessKilledDaoTest.java
@@ -0,0 +1,34 @@
+package org.seckill.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by grant on 2017/6/4.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/spring-dao.xml"})
+public class SuccessKilledDaoTest {
+
+    @Resource
+    private SuccessKilledDao successKilledDao;
+
+    @Test
+    public void insertSuccessKilled() throws Exception {
+        long id = 1000L;
+        long phone = 13888888888L;
+        int insertCount = successKilledDao.insertSuccessKilled(id, phone);
+        System.out.println("insertCount=" + insertCount);
+    }
+
+    @Test
+    public void queryByIdWithSeckill() throws Exception {
+    }
+
+}
